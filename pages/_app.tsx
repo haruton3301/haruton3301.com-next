03619dd5a7ff7e718f5c01b53f42ae7570e4340c
Haruto Ikehara <58582197+kduxwr@users.noreply.github.com>
2022-08-29 18:39:55 +0900

    taildwindを導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import React from 'react'
+import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <button className="btn btn-primary">Hello</button>
+    </>
+  )
+}
